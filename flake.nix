8cbfa4cfb9d4b67b7628526f103211140176b7c8
COTHSC <thierryscully.colomban@gmail.com>
2025-11-08 23:00:01 +0100

    .

--- flake.nix
+++ flake.nix
@@ -24,6 +24,7 @@
                 nerd-fonts.jetbrains-mono
                 nerd-fonts.fira-code
                 nerd-fonts.hack
+                pass
             ];
 
             programs.direnv = {
